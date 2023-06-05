efb4f0792a33cd7c63aad1e21e65869ba2141d46
menriquez202002020 <82660602+menriquez202002020@users.noreply.github.com>
2023-06-05 15:08:05 +0800

    Add files via upload

---
+++ src/components/Canvas/Canvas.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import ColorTemplate from '../ColorTemplate/ColorTemplate'
+import { initialState } from './../InitialState/InitialState'
+import WaveCanvas from '../WaveCanvas/WaveCanvas'
+import './Canvas.css'
+
+
+interface CanvasProps {
+  id: string
+  audioBuffer: AudioBuffer
+  waveHeight: number
+  width: number
+  height: number
+}
+
+const Canvas: React.FC<CanvasProps> = ({ id, waveHeight, audioBuffer, width, height }) => {
+  const [selectedColor, setSelectedColor] = useState<string>('Frame')
+  const [canvasTitle, setCanvasTitle] = useState<string>('Enter your title')
+  const [canvasSubtitle, setCanvasSubtitle] = useState<string>('Enter your subtitle here')
+  const [canvasSoundwave, setCanvasSoundwave] = useState<string>('')
+  const handleFrameSelection = (value: string): void => {
+    setSelectedColor(value)
+    console.log(value)
+  }
+  return (
+    <>
+      <div className='canvas-component'>
+        <div className='canvas-header'>
+          <p>Landscape Image Background Template</p><img src='src/assets/icons/header-icon.png' alt='' />
+        </div>
+        <div className='canvas-content'>
+          <div className="canvas-text title">
+            <h1>{ canvasTitle }</h1>
+          </div>
+          <div className="canvas-text subtitle">
+            <h1>{ canvasSubtitle }</h1>
+          </div>
+          <div className="canvas-soundwave">
+          <WaveCanvas id={id} waveHeight={waveHeight} audioBuffer={audioBuffer} width={width} height={height} />
+          </div>
+        </div>
+        <div className='canvas-footer desktop'>
+          <ColorTemplate options={initialState.colorOptions} handleFrameSelection={handleFrameSelection}/>
+        </div>
+        <div className='canvas-footer mobile'>
+          <ColorTemplate options={initialState.colorOptionsMobile} handleFrameSelection={handleFrameSelection}/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Canvas
---
+++ src/components/ColorTemplate/ColorTemplate.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import './ColorTemplate.css'
+
+interface ToggleButtonProps {
+  options: Array<{ key: string, image: string, view: string }>
+  handleColorSelection: (value: string) => void
+}
+
+const ColorTemplate: React.FC<ToggleButtonProps> = ({ options, handleColorSelection }) => {
+  const [selectedOption, setSelectedOption] = useState('option_0')
+  const handleOptionChange = (value: any): any => {
+    setSelectedOption(value.key)
+    handleColorSelection(value.view+" "+value.key)
+  }
+  return (
+    <>
+        <div className='template-color'>
+            {options.map((option: any) => (
+                <label className={`col-3 frame-color-selection frame-color ${ option.view } ${selectedOption === option.key && 'active'}`} key={option.key}>
+                <input
+                    type="radio"
+                    value={option.key}
+                    checked={selectedOption === option.key}
+                    onChange={() => handleOptionChange(option)}
+                />
+                <img src={option.image} alt={option.key} />
+                </label>
+            ))}
+        </div>
+    </>
+  )
+}
+
+export default ColorTemplate
