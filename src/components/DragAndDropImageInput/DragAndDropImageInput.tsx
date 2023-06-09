b01ac774507c1dd6032922ee800d59a9fcc2e213
Mark <m.enriquez02020@gmail.com>
2023-06-09 15:08:59 +0800

    updates 6/9/23

---
+++ src/components/DragAndDropImageInput/DragAndDropImageInput.tsx
@@ -0,0 +1,59 @@
+import React, { useRef, useState } from 'react'
+import './DragAndDropImageInput.css'
+
+interface DragAndDropInputProps {
+  onImageChange: (file: File) => void
+}
+
+interface DragAndDropInputState {
+  isDragging: boolean
+}
+
+const DragAndDropImageInput: React.FC<DragAndDropInputProps> = ({ onImageChange }) => {
+  const [state, setState] = useState<DragAndDropInputState>({
+    isDragging: false
+  })
+  const inputRef = useRef <any>()
+
+  const handleDragEnter = (event: React.DragEvent<HTMLInputElement>): void => {
+    event.preventDefault()
+    setState({ isDragging: true })
+  }
+
+  const handleDragLeave = (event: React.DragEvent<HTMLInputElement>): void => {
+    event.preventDefault()
+    setState({ isDragging: false })
+  }
+
+  const handleDrop = (event: React.DragEvent<HTMLInputElement>): void => {
+    event.preventDefault()
+    const { files } = event.dataTransfer
+    if (files.length > 0) {
+      onImageChange(files[0])
+    }
+    setState({ isDragging: false })
+  }
+
+  return (
+    <div
+      className={`drag-and-drop-input ${state.isDragging ? 'dragging' : ''}`}
+      onDragEnter={handleDragEnter}
+      onDragOver={handleDragEnter}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
+    >
+      <img src='../src/assets/icons/img-upload.png' alt='' onClick={() => inputRef.current.click()} className='plus-icon' />
+      <input
+        type='file'
+        accept='img/*'
+        onChange={(event) => { onImageChange(event.target.files?.[0]) }}
+        hidden
+        ref={inputRef}
+      />
+      <p onClick={() => inputRef.current.click()}>Upload or drag & drop your image</p>
+      <span>JPG, JPEG, PNG - 5MB</span>
+    </div>
+  )
+}
+
+export default DragAndDropImageInput
--- src/pages/customizer/customizer.tsx
+++ src/pages/customizer/customizer.tsx
@@ -1,6 +1,7 @@
 import Canvas from './../../components/Canvas/Canvas'
 import React, { useCallback, useEffect, useState } from 'react'
 import { Accordion } from 'react-bootstrap'
+import DragAndDropImageInput from '../../components/DragAndDropImageInput/DragAndDropImageInput'
 import DragAndDropInput from '../../components/DragAndDropInput/DragAndDropInput'
 import WaveCanvas from '../../components/WaveCanvas/WaveCanvas'
 import { initialState } from '../../components/InitialState/InitialState'
@@ -14,13 +15,16 @@ export const Customizer: React.FC = () => {
   const [audioFile, setAudioFile] = useState<File | null>(null)
   const [audioBuffer, setAudioBuffer] = useState<AudioBuffer | null>(null)
   const [showFileSizeAlert, setShowFileSizeAlert] = useState<boolean>(false)
+  const [showImageSizeAlert, setShowImageSizeAlert] = useState<boolean>(false)
   const [audioFileName, setAudioFileName] = useState<string>('No Files Selected')
   const [selectedFrame, setSelectedFrame] = useState<string>('')
   const [selectedSizing, setSelectedSizing] = useState<string>('')
   const [selectedColor, setSelectedColor] = useState<string>('')
   const [canvasTitle, setCanvasTitle] = useState<string>('Enter your title')
   const [canvasSubtitle, setCanvasSubtitle] = useState<string>('Enter your subtitle here')
+  const [editLayoutBackground, setEditLayoutBackground] = useState<boolean>(false)
   const [showConfirmation, setShowConfirmation] = useState(false)
+  const [layoutBackgroundImage, setLayoutBackgroundImage] = useState(initialState.defaultLayoutBackgroundImage)
 
   const handleAudioChange = (file: File): void => {
     if (typeof file !== 'undefined' || file !== null) {
@@ -33,6 +37,22 @@ export const Customizer: React.FC = () => {
       } else {
         setShowFileSizeAlert(true)
         setAudioFile(null)
+      }
+    }
+  }
+  const handleLayoutImageUpdate = (file: File): void => {
+   
+    if (typeof file !== 'undefined' || file !== null) {
+      const fileSizeInMB = file.size / (1024 * 1024)
+      if (fileSizeInMB <= 5) {
+        const imageReader = new FileReader();
+            imageReader.readAsDataURL(file);
+            imageReader.onloadend = () => {
+              setLayoutBackgroundImage(imageReader.result);
+              setShowImageSizeAlert(false)
+            };
+      } else {
+        setShowImageSizeAlert(true)
       }
     }
   }
@@ -53,6 +73,10 @@ export const Customizer: React.FC = () => {
     },
     [audioFile]
   )
+  const onImageClick = (event: Event): void => {
+    setEditLayoutBackground(true)
+    console.log(event)
+  }
   const resetAudioFile = (): void => {
     setShowConfirmation(true)
     console.log(showConfirmation)
@@ -65,7 +89,13 @@ export const Customizer: React.FC = () => {
     setSelectedSizing(value)
     console.log(value)
   }
+  const handleCloseEditLayoutBackground = (): void => {
+    setEditLayoutBackground(false)
+  }
   const handleColorSelection = (value: string): void => {
+    if (selectedColor !== ''){
+      setEditLayoutBackground(true)
+    }
     setSelectedColor(value)
     console.log(value)
   }
@@ -91,16 +121,42 @@ export const Customizer: React.FC = () => {
     [audioFile, selectedFrame, selectedSizing, showConfirmation]
   )
   return (
-    <>
-      <div className='template-container'>
-        <div className='col-12 customizer-container'>
-          <div className='col-4 input-container'>
-            <Accordion defaultActiveKey={['0']} >
-              <Accordion.Item eventKey='0'>
-                <Accordion.Header><div className='upload-header'><div><img src='src/assets/icons/upload.png' alt='icon' /> Upload </div><p className='upload-desc'>Upload yuor media to continue:</p></div></Accordion.Header>
-                <Accordion.Body>
+  <>
+    <div className='template-container'>
+      <div className='col-12 customizer-container'>
+        <div className='col-4 input-container'>
+        { editLayoutBackground ?
+        <Accordion defaultActiveKey={['0']} className={`main-accordion-layout`}>
+          <Accordion.Item eventKey='0'>
+            <Accordion.Header className={`upload-header ${audioBuffer !== null ? 'file-uploaded' : ''}`}><div className='upload-header'><div>Background customization </div><p className='upload-desc'>Style soundwave of your soundwave art</p></div></Accordion.Header>
+            <Accordion.Body>
+                  <div className='accordion-upload-container image-container'>
+                    <div className='upload-container image-container'>
+                      <DragAndDropImageInput onImageChange={handleLayoutImageUpdate} />
+                    </div>
+                    {layoutBackgroundImage && !showImageSizeAlert ?
+                      <div className='background-image-container'>
+                        <button className="btn btn-primary w-100" onClick={handleCloseEditLayoutBackground}>
+                          Save and Close
+                        </button>
+                      </div> 
+                    : null }
+                  </div>
+                  {showImageSizeAlert ?
+                    <div className='alert-container'>
+                      <p><img src='src/assets/icons/Check_ring_light.png' alt='' />{'Background size should not exceed 5MB.'}</p>
+                    </div>
+                    : null
+                  }
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+        :
+        <Accordion defaultActiveKey={['0']} className={`main-accordion-layout`}>
+          <Accordion.Item eventKey='0'>
+            <Accordion.Header className={`upload-header ${audioBuffer !== null ? 'file-uploaded' : ''}`}><div className='upload-header'><div><img src='src/assets/icons/upload.png' alt='icon' /> Upload </div><p className='upload-desc'>Upload yuor media to continue:</p></div></Accordion.Header>
+            <Accordion.Body>
                   <div className='accordion-upload-container'>
-
                     {(audioBuffer !== null) &&
                       <div className='upload-wave-container'>
                         <WaveCanvas id='acc_sound_wave' waveHeight={initialState.waveHeight} audioBuffer={audioBuffer} width={initialState.canvasWidth} height={initialState.canvasHeight} />
@@ -108,7 +164,6 @@ export const Customizer: React.FC = () => {
                           <img src='src/assets/icons/play-icon.png' alt='' />
                           <p className='audio-name'>{audioFileName}</p>
                           <img src='src/assets/icons/delete-icon.png' onClick={resetAudioFile} alt='' />
-
                         </div>
                       </div>}
                     {(audioBuffer === null) && <div className='upload-container'><DragAndDropInput onFileChange={handleAudioChange} /></div>}
@@ -118,65 +173,79 @@ export const Customizer: React.FC = () => {
                       <p><img src='src/assets/icons/Check_ring_light.png' alt='' />{'Media size should not exceed 10MB.'}</p>
                     </div>
                   }
-                </Accordion.Body>
-              </Accordion.Item>
+            </Accordion.Body>
+          </Accordion.Item>
               <Accordion.Item eventKey='1'>
-                <Accordion.Header><img src='src/assets/icons/material-sizing.png' alt='icon' /> Material & Sizing</Accordion.Header>
-                <Accordion.Body>
-                  <div className="material-and-sizing-container">
-                    <p>Frame Type</p>
-                    <RadioButtonToggle options={initialState.frameOptions} handleFrameSelection={handleFrameSelection} />
-                    <p>Size</p>
-                    <LayoutSizing options={initialState.sizingOptions} handleSizingSelection={handleSizingSelection} />
-                  </div>
-                </Accordion.Body>
-              </Accordion.Item>
+            <Accordion.Header className={`material-and-sizing-header ${(selectedFrame !== '' && selectedSizing !== '' && audioBuffer !== null) ? 'material-sizing-selected' : ''}`} ><div className='upload-header'><div><img src='src/assets/icons/material-sizing.png' alt='icon' /> Material & Sizing</div><p className='upload-desc'>Upload yuor media to continue:</p></div></Accordion.Header>
+            <Accordion.Body>
+              <div className="material-and-sizing-container">
+                <p>Frame Type</p>
+                <RadioButtonToggle options={initialState.frameOptions} handleFrameSelection={handleFrameSelection}/>
+                <p>Size</p>
+                <LayoutSizing options={initialState.sizingOptions} handleSizingSelection={handleSizingSelection}/>
+              </div>
+            </Accordion.Body>
+          </Accordion.Item>
               <Accordion.Item eventKey='2'>
-                <Accordion.Header><img src='src/assets/icons/preview.png' alt='icon' /> Order Review</Accordion.Header>
-                <Accordion.Body>
-                  Order Preview
-                </Accordion.Body>
-              </Accordion.Item>
-            </Accordion>
-            <ConfirmationModal
-              isOpen={showConfirmation}
-              message="Are you sure you want to remove the audio you uploaded?"
-              subMessage="You will not be able to undo this action."
-              onConfirm={handleConfirmDelete}
-              onCancel={handleCancelDelete}
-              style={{ display: showConfirmation ? 'block' : 'none' }}
-            />
-          </div>
-          <div className='col-8 canvas-container'>
-            <div className='canvas-component'>
-              <div className='canvas-header'>
-                <p>Landscape Image Background Template</p><img src='src/assets/icons/header-icon.png' alt='' />
+            <Accordion.Header><div className='upload-header'><div><img src='src/assets/icons/preview.png' alt='icon' /> Order Review</div><p className='upload-desc'>Here is your order details:</p></div></Accordion.Header>
+            <Accordion.Body>
+             <ul className="order-preview-container">
+              <li className="order-item">
+                ORIENTATION<strong>{'Landscapre'}</strong>
+              </li>
+              <li className="order-item">
+                FRAME TYPE<strong>{selectedFrame}</strong>
+              </li>
+              <li className="order-item">
+                SIZE<strong>{selectedSizing}</strong>
+              </li>
+              <li className="order-item">
+                TOTAL PRICE<strong>{'€50.00'}</strong>
+              </li>
+             </ul>
+            </Accordion.Body>
+          </Accordion.Item>
+          </Accordion>
+          }
+          <ConfirmationModal
+            isOpen={showConfirmation}
+            message="Are you sure you want to remove the audio you uploaded?"
+            subMessage="You will not be able to undo this action."
+            onConfirm={handleConfirmDelete}
+            onCancel={handleCancelDelete}
+            style={{ display: showConfirmation ? 'block' : 'none' }}
+          />
+        </div>
+        <div className='col-8 canvas-container'>
+          <div className='canvas-component'>
+            <div className='canvas-header'>
+              <p>Landscape Image Background Template</p><img src='src/assets/icons/header-icon.png' alt='' />
+            </div>
+            <div className={`canvas-content`} style = {{background: `url('${layoutBackgroundImage}'`}}>
+              <div className={`overlay ${selectedColor}`}></div>
+              <div className="canvas-text title">
+                <h1>{ canvasTitle }</h1>
               </div>
-              <div className='canvas-content'>
-                <div className="canvas-text title">
-                  <h1>{canvasTitle}</h1>
-                </div>
-                <div className="canvas-text subtitle">
-                  <h1>{canvasSubtitle}</h1>
-                </div>
-                <div className="canvas-soundwave">
-                  {(audioBuffer !== null) ?
-                    <WaveCanvas id='canvas-canvas' waveHeight={initialState.waveHeight} audioBuffer={audioBuffer} width={initialState.canvasWidth} height={initialState.canvasHeight} />
-                    : <div className="temp-canvas-image"><img src="src/assets/img/soundwave.png" alt="" /></div>
-                  }
-                </div>
+              <div className="canvas-text subtitle">
+                <h1>{ canvasSubtitle }</h1>
               </div>
-              <div className='canvas-footer desktop'>
-                <ColorTemplate options={initialState.colorOptions} handleColorSelection={handleColorSelection} />
-              </div>
-              <div className='canvas-footer mobile'>
-                <ColorTemplate options={initialState.colorOptionsMobile} handleColorSelection={handleColorSelection} />
+              <div className="canvas-soundwave">
+                {(audioBuffer !== null)
+                  ? <WaveCanvas id='canvas-canvas' waveHeight={initialState.waveHeight} audioBuffer={audioBuffer} width={initialState.canvasWidth} height={initialState.canvasHeight} />
+                  : <div className="temp-canvas-image"><img src="src/assets/img/soundwave.png" alt="" /></div>
+                }
               </div>
             </div>
-
+              <div className='canvas-footer desktop'>
+                <ColorTemplate options={initialState.colorOptions} onImageClick={onImageClick} handleColorSelection={handleColorSelection}/>
+              </div>
+              <div className='canvas-footer mobile'>
+                <ColorTemplate options={initialState.colorOptionsMobile} onImageClick={onImageClick} handleColorSelection={handleColorSelection}/>
+              </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
+  </>
   )
 }
